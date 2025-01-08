a65a919bc7d5cfda2b20a7fd7fa6e2a82be9bd11
ItsJESH <jenishpaghadalin018@gmail.com>
2025-01-08 10:00:43 +0530

    v0.0.2

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// import ollama from "ollama";
+
+// askQuestion()
+// async function askQuestion() {
+//   try {
+//     const response = await ollama.chat({
+//       model: "llama3.2:1b",
+//       messages: [{ role: 'user', content: "Write a short story plot" }]
+//     });
+
+//     console.log("Chatbot Response:", response.message.content);
+//   } catch (error) {
+//     console.error("Error occurred:", error.message);
+//   }
+// }
+
+
+// ------------------------------------------------------------------
+//Stage 2:
+// ------------------------------------------------------------------
+
 import ollama from "ollama";
 import fs from "fs";
 
